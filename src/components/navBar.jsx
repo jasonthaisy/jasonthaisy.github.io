ffa57629e838e4c5eceb37d865945b3d97055f4c
jasonthaisy <jtsy@gmail.com>
2019-10-19 01:10:30 -0700

    Added NavBar

---
+++ src/components/navBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="mx-auto fade" style={{ width: 200 }}>
+        <h1>Jason Sy</h1>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
